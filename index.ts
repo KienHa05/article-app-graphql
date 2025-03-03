e3fd72b1cec2d321d7e787647019113fb15fab5b
KienHa05 <kienhgph50150@gmail.com>
2025-03-03 16:20:47 +0700

    6.1 Cấu trúc lại file typeDefs.ts
    BUỔI 59

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 import * as database from "./config/database";
 import { ApolloServer, gql } from "apollo-server-express";
 
-import { typeDefs } from "./typeDefs";
 import { resolvers } from "./resolvers";
+import { typeDefs } from "./typeDefs/index.typeDefs";
 
 const startServer = async () => {
     dotenv.config();
@@ -16,7 +16,7 @@ const startServer = async () => {
 
     // GraphQL
     const apolloServer = new ApolloServer({
-        typeDefs,
+        typeDefs: typeDefs,
         resolvers
     });
 
